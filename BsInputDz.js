6bc685228c5d3005699f7f150cef61d727a65df3
RenatHuseynzadeh <124487433+RenatHuseynzadeh@users.noreply.github.com>
2023-11-07 19:08:05 +0300

    Add files via upload

---
+++ BsInputDz.js
@@ -0,0 +1,20 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+
+
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+// const forms = document.querySelector('.needs-validation')
+
+// Loop over them and prevent submission
+
+// const hhh = document.querySelector('.vvv')
+// 
+// form.addEventListener(hhh, (event) => {
+    // if (!form.checkValidity()) {
+        // event.preventDefault()
+        // event.stopPropagation()
+    // }
+// 
+    // form.classList.add('was-validated')
+// }, false)
+
